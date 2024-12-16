51f4d981154c0c2794b9b109d29446288e8bab56
Saanvi Kumbhare <kumbharesaanvi@gmail.com>
2024-12-16 22:03:30 +0530

    Update first.js & index.html

--- JavaScript/first.js
+++ JavaScript/first.js
@@ -1,9 +1,1 @@
-const profile={
-    username: "Nezuko-chan",
-    posts: 220,
-    followers: 600000,
-    following: 4,
-    bio: "I am Tanjiro's little sis",
-    isFollow: false,
-};
-console.log(typeof profile.isFollow);
+console.log(Number.parseFloat("3.14"))
