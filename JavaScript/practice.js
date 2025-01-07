e38ee9040cbe51fe97d1d3c1b5c511ab5cd83fba
Saanvi Kumbhare <kumbharesaanvi@gmail.com>
2025-01-07 22:40:54 +0530

    Updated practice.js

--- JavaScript/practice.js
+++ JavaScript/practice.js
@@ -1,11 +1,46 @@
-/*for(let i=0; i<=100; i++){
+/*For loop example
+
+for(let i=0; i<=100; i++){
     if(i%2==0){
         console.log(i);
     }
-}*/
+}
 
+*/
+
+/*Switch case example
+
+let fruits= prompt("We have Apple Banana Orange Pomegranate Papaya, what do you want?")
+
+switch (fruits){
+    case "apple":
+        console.log("2$ per apple");
+        break;
+    
+    case "banana":
+        console.log("1$ per banana");
+        break;
+    
+    case "orange":
+        console.log("2$ per orange");
+        break;
+
+    case "pomegranate":
+        console.log("2$ per pomegranate");
+        break;
+
+    case "papaya":
+        console.log("5$ per papaya");
+        break;
+}
+
+*/
+
+/*userID exmaple
 const prompt=require('prompt-sync')();
 
 let username=prompt("Enter your username without spaces: ");
 userID=`@${username}${username.length}`;
 console.log(userID);
+
+*/
