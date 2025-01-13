05ebd78f67d178089c4645c3bd577a9081141f5c
Saanvi Kumbhare <kumbharesaanvi@gmail.com>
2025-01-14 00:28:56 +0530

    Added some lines in app.js

--- React/app.js
+++ React/app.js
@@ -1,8 +1,12 @@
-const require = require("express");
+const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+app.get("/about", (req, res) => {
+  res.send("About Page");
 });
 
 app.listen(7000, () => {
