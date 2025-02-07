9299fa90c19ed97d0798c17b34fa72248eace493
Saanvi <kumbharesaanvi@gmail.com>
2025-02-07 05:13:51 +0000

    Added Counter to Greeting.js

--- React/my-first-react-app/src/Greeting.js
+++ React/my-first-react-app/src/Greeting.js
@@ -1,9 +1,12 @@
 import React from "react";
 
 function Greeting(props) {
+  const [count,setCount] = useState(0); 
   return (
     <div>
       <h2>Hello, {props.name}!</h2>
+      <p>You've clicked {count} times.</p>
+      <button onClick={()=> setCount(count+1)}> Click me </button>
     </div>
   );
 }
